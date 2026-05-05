25eebc6b738b441435e2d14624ba309453f3e24e
Remsiti <remsitimesrapurba@gmail.com>
2026-05-05 21:24:26 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\pasienController;
 
 
-Route::get('/', function () {return view('layout.app'); });
-Route::get('/pasien', [pasienController::class, 'index'])->name('sistem.index');
-Route::get('/pasien/create', [pasienController::class, 'create'])->name('sistem.create');
-Route::post('/pasien/store', [pasienController::class, 'store'])->name('sistem.store');
+// Route::get('/', function () {return view('layout.app'); });
+// Route::get('/pasien', [pasienController::class, 'index'])->name('sistem.index');
+// Route::get('/pasien/create', [pasienController::class, 'create'])->name('sistem.create');
+// Route::post('/pasien/store', [pasienController::class, 'store'])->name('sistem.store');
 
-// Edit Buku
-Route::get('/buku/{id}/edit', [pasienController::class, 'edit'])->name('sistem.edit');
-Route::put('/buku/{id}', [pasienController::class, 'update'])->name('sistem.update');
 
-Route::delete('/buku/{id}', [pasienController::class, 'destroy'])->name('sistem.destroy');
+// Route::get('/buku/{id}/edit', [pasienController::class, 'edit'])->name('sistem.edit');
+// Route::put('/buku/{id}', [pasienController::class, 'update'])->name('sistem.update');
+
+// Route::delete('/buku/{id}', [pasienController::class, 'destroy'])->name('sistem.destroy');
+Route::get('/', function () {
+    return view('layout.app');
+});
+
+Route::resource('sistem', pasienController::class);
